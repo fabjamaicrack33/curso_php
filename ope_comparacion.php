362287edf16217c6ed1931869dbca66750333862
Fabian J Vargas <121529365+fabjamaicrack33@users.noreply.github.com>
2023-04-23 21:05:38 -0500

    se ingresan operadores en PHP curso ok

---
+++ ope_comparacion.php
@@ -0,0 +1,26 @@
+<?php
+$a = "20";
+$b = 20;
+
+echo $a <  $b;
+echo '<br>';
+
+echo $a >  $b;
+echo '<br>';
+
+echo $a <= $b;
+echo '<br>';
+
+echo $a >=  $b;
+echo '<br>';
+
+echo $a == $b;
+echo '<br>';
+
+echo $a === $b;
+echo '<br>';
+
+echo $a != $b;
+echo '<br>';
+
+?>
